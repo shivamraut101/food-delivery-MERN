1c9a8073171cc50d075e2808e2171e55f53f53ed
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-11 20:24:16 +0530

    index.js for backend users route api added

---
+++ Big Basket/backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 5000
+const mongooseMongoDB = require('./db')
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  })
+  
+mongooseMongoDB()
+app.get('/',(req,res)=>{
+    res.send("Hii I am Shivam")
+})
+
+app.use(express.json())
+app.use('/api',require("./Routes/CreateUser"))
+
+app.listen(port,()=>{
+    console.log("Express is listening ", port)
+})
