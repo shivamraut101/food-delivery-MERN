95ac9ee93ff29cdcde1c2db21548ee041424878d
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-11 20:29:36 +0530

    DisplayData api added

--- Big Basket/backend/index.js
+++ Big Basket/backend/index.js
@@ -17,6 +17,7 @@ app.get('/',(req,res)=>{
 
 app.use(express.json())
 app.use('/api',require("./Routes/CreateUser"))
+app.use('/api',require("./Routes/DisplayData"))
 
 app.listen(port,()=>{
     console.log("Express is listening ", port)
